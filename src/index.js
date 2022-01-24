0cbb74c6c523807bbc6b3b7be9e30a455abe3219
“Radik <radik.zainullin@gmail.com>
2022-01-24 12:56:33 +0300

    задача решена

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-// Перед выполнение этого задания рекомендовано ознакомиться с документацией метод Array.prototype.reverse:
-// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/reverse
-// Напишите полифил метода Array.prototype.reverse. В теле полифила не используйте никаких методов класса Array или других классов. Используйте только условные операторы и циклы.
+// Перед выполнение этого задания рекомендовано ознакомиться с документацией метод Array.prototype.join:
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/join
+// Напишите полифил метода Array.prototype.join. В теле полифила не используйте никаких методов класса Array или других классов. Используйте только условные операторы и циклы.
 // В полифиле первый аргумент - это массив. Все остальные аргументы такие же как и в том же порядке, что и у одноименного метода. Т.е.:
-// array.reverse()  <=> reverse(array)
+// array.join(separator)  <=> join(array, separator)
 
-function reverse(arr) {
-  const result = [];
-  while (arr.length > 0) {
-    result.push(arr.pop());
+function join(array, separator = ',') {
+  let str = '';
+  for (let i = 0; i < array.length; i += 1) {
+    if (i === array.length - 1) {
+      str = str + String(array[i]);
+    } else {
+      str = str + String(array[i]) + separator;
+    }
   }
-  while (result.length > 0) {
-    arr.push(result.shift());
-  }
-  return arr;
+  return str;
 }
 
-
 module.exports = {
-  reverse,
+  join,
 };
--- test/index.test.js
+++ test/index.test.js
@@ -1,30 +1,31 @@
-const { reverse } = require('../src/index');
+const { join } = require('../src/index');
 
-describe("Тестирование метода Array.prototype.fill", () => {
+describe("Тестирование метода Array.prototype.join", () => {
 	it("Тест 1", () => {
-		const numbers = [];
+		const numbers = [1, 2, 3];
 
-		const result = reverse(numbers);
-
-		expect(numbers).toEqual([]);
-		expect(result).toBe(numbers);
+		expect(join(numbers)).toBe("1,2,3");
+		expect(numbers.join()).toBe("1,2,3");
 	});
 
 	it("Тест 2", () => {
 		const numbers = [1, 2, 3];
 
-		const result = reverse(numbers);
-
-		expect(numbers).toEqual([3, 2, 1]);
-		expect(result).toBe(numbers);
+		expect(join(numbers, ",")).toBe("1,2,3");
+		expect(numbers.join(",")).toBe("1,2,3");
 	});
 
 	it("Тест 3", () => {
-		const numbers = [1, 2, 3, 4];
+		const numbers = [1, 2, 3];
 
-		const result = reverse(numbers);
+		expect(join(numbers, " ")).toBe("1 2 3");
+		expect(numbers.join(" ")).toBe("1 2 3");
+	});
 
-		expect(numbers).toEqual([4, 3, 2, 1]);
-		expect(result).toBe(numbers);
+	it("Тест 4", () => {
+		const numbers = [1, 2, 3];
+
+		expect(join(numbers, " !! 123 !! ")).toBe("1 !! 123 !! 2 !! 123 !! 3");
+		expect(numbers.join(" !! 123 !! ")).toBe("1 !! 123 !! 2 !! 123 !! 3");
 	});
 });
